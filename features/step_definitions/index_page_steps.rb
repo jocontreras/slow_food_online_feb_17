b46cd30d90b17c77bef7000661793574a97e438f
jocontreras <jen.almarasy@gmail.com>
2017-03-29 16:01:47 +0200

    add ffaker gem and use for viewing the list or restaurants

--- features/step_definitions/index_page_steps.rb
+++ features/step_definitions/index_page_steps.rb
@@ -1,3 +1,9 @@
 Given(/^I am on the index page$/) do
   visit root_path
 end
+
+Given(/^the following restaurant exists$/) do |table|
+  table.hashes.each do |hash|
+   Restaurant.create!(hash)
+  end
+end
